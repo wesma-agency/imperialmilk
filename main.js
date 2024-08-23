7428aa4bf1732db926c9f5748eb9a8d79455fdd6
Alex Levchuk <alexlevchuk@mail.ru>
2024-08-23 13:49:23 +0300

    DEV 77406: добавил слайдер

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './src/scss/style.scss';
+import Swiper from 'swiper/bundle';
+
+const slider = new Swiper('.swiper', {
+	// loop: true,
+	// autoplay: {
+	// 	delay: 8000,
+	// },
+	// keyboard: {
+	// 	enable: true,
+	// },
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
