7598983bf69fdcbfb4c89ad64af8256d6ef1528c
Alex Levchuk <alexlevchuk@mail.ru>
2024-08-26 11:37:17 +0300

    DEV 77406: добавлена секция наша продукция

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 import './src/scss/style.scss';
 import Swiper from 'swiper/bundle';
 
-const slider = new Swiper('.swiper', {
-	// loop: true,
-	// autoplay: {
-	// 	delay: 8000,
-	// },
-	// keyboard: {
-	// 	enable: true,
-	// },
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	pagination: {
-		el: '.swiper-pagination',
-	},
+window.addEventListener('DOMContentLoaded', () => {
+	createSlider('.swiper', {
+		// loop: true,
+		// autoplay: {
+		// 	delay: 8000,
+		// },
+		// keyboard: {
+		// 	enable: true,
+		// },
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
 });
+
+// const slider = new Swiper('.swiper', {
+// 	// loop: true,
+// 	// autoplay: {
+// 	// 	delay: 8000,
+// 	// },
+// 	// keyboard: {
+// 	// 	enable: true,
+// 	// },
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 		clickable: true,
+// 	},
+// });
+
+function createSlider(slider, options = {}) {
+	return new Swiper(`${slider}`, options);
+}
